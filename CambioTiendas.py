ea3b9e5e565ef27401922c153dc94566acd799ce
Rofer <roferhn@gmail.com>
2023-08-11 09:43:41 -0600

    Optimize querys

--- CambioTiendas.py
+++ CambioTiendas.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import psycopg2
-
-
-
+import datetime
 
 # ---VARIABLES---#
 Tiendas = ["BK", "LC", "CH", "PC", "PP", "CC", "CK", "DD", "BK-CALL", "LC-CALL", "CH-CALL", "PC-CALL", "PP-CALL", "CC-CALL", "CK-CALL", "DD-CALL",]
@@ -15,15 +13,15 @@ def leer_Reporte(nombreReporte):
         df_reporte = pd.read_excel(nombreReporte, engine='xlrd')
         return df_reporte
     except:
-        return "---","Archivo no encontrado"
+        return "--- Archivo no encontrado"
 
 # ---Convierte el archivo de FAC en un dataframe---#
-def leer_FAC(nombreFac):
+def leer_FAC(nombreFac):      
     try:
-        df_reporte = pd.read_excel(nombreFac)
+        df_reporte = pd.read_excel(nombreFac, engine='xlrd')
         return df_reporte
     except:
-        return "---","Archivo no encontrado"
+        return "--- Archivo no encontrado"
 
 # ---obtiene los numeros de autorizacion---#
 def obtener_authNum(numFila, dfReporte):
@@ -47,28 +45,19 @@ def Obtener_orderId(authNum, dfFAC):
     try:
         index= dfFAC[dfFAC["Auth Code"] == authNum].index[0]
         OrderId= dfFAC.at[index, "Order ID"]
-        return OrderId
+        
+        return int(OrderId)
     except:
-        print("!!!","Auth_Num ", auth_Num, " no encontrado en FAC")
-        return False
+        #print("!!!Auth_Num "+str(auth_Num)+ " no encontrado en FAC")
+        return "!!!Auth_Num "+str(auth_Num)+ " no encontrado en FAC"
 
 #---Concectar a la DB y obtener tienda---#
 def Obtener_Tienda(orderId):
-    host = 'avecsm.cksl0gxfpv9j.us-east-2.rds.amazonaws.com'
-    database =  'AVECSM'
-    user = 'itintur'
-    password = 'Intur#2021!'
-
-    #---Conectar a la DB---#
-    conecction = psycopg2.connect(host=host, database=database, user=user, password=password)
-    cursor = conecction.cursor()
 
     if orderId >= 1000000:
         orderFull = "000" + str(orderId)
-        print(orderFull)
     else: 
         orderFull = "0000" + str(orderId)
-        print(orderFull)
 
     query= "SELECT cmbpa.username as Tienda FROM public.csm_prc_order cpo LEFT JOIN cs_mst_business_partner_address cmbpa ON cmbpa.id = CAST(cpo.provider AS INTEGER) WHERE cpo.code = '" + str(orderFull) + "'"
 
@@ -82,9 +71,6 @@ def Obtener_Tienda(orderId):
             cursor.execute(str(query))
             tienda = cursor.fetchone()[0]
 
-            cursor.close()
-            conecction.close()
-
             if tienda[:2] == "IJ":
                 return "CL" + str(tienda[-2:])
             else:
@@ -93,38 +79,69 @@ def Obtener_Tienda(orderId):
     except:
         return "No se pudo obtener tienda de la orden: " + str(orderId)
 
-
-
 # ---------------------------------------------------------------------------#
-print("Code by Rofer")
-#Reporte = "VNP-05072023" + ".xls"
-Reporte = input("Nombre del reporte?") + ".xls"
+# ---------------------------------------------------------------------------#
 
-#FAC = "Transactions_20230705_135000"+".xlsx"
-FAC = input("Nombre del archivo de FAC?")+".xls"
-i = 0
+print("Code by Rofer")
+Reporte = input("Ingrese el nombre del reporte: ") + ".xls"
+FAC = input("Ingrese el nombre del archivo de FAC:  ")+".xls"
+
+fecha_actual = datetime.datetime.today().strftime('%Y-%m-%d')
+Log = f"Tiendas_{fecha_actual}.txt"
+
+#---DB data---#
+host = ''
+database =  ''
+user = ''
+password = ''
+
+
+#---Conectar a la DB---#
+conecction = psycopg2.connect(host=host, database=database, user=user, password=password)
+cursor = conecction.cursor()
+
 df_Reporte = leer_Reporte(Reporte)
 df_FAC = leer_FAC(FAC)
 
-while i < df_Reporte.shape[0]:
+i = 0
 
-    auth_Num = obtener_authNum(i, df_Reporte)
+if isinstance(df_Reporte, str) or isinstance(df_FAC, str):
+    print("---Archivo no encontrado")
+    input("Presiona Enter para salir...")
 
-    if auth_Num is None:
+else:
+    with open(Log, "w") as archivo:
         pass
-    else:
-        Order_id = Obtener_orderId(auth_Num,df_FAC)
 
+    while i < df_Reporte.shape[0]:
 
-        if Order_id==False:
+        auth_Num = obtener_authNum(i, df_Reporte)
+
+        if auth_Num is None:
             pass
         else:
-            Tienda = Obtener_Tienda(Order_id)
-            print(auth_Num + " / " + str(Order_id) + " / " + Tienda)
 
-            #---Cambiar Tienda---#
-            df_Reporte.at[i,"RESTAURANTE"] = Tienda
-            df_Reporte.at[i,"OBS"] = ""
+            Order_id = Obtener_orderId(auth_Num,df_FAC)
+            if isinstance(Order_id, str):
+                print(Order_id)
+                with open(Log, "a") as archivo:
+                    archivo.write(Order_id + "\n")
 
-    i += 1
-df_Reporte.to_excel(Reporte[:-4]+"Nuevo"+".xlsx",index=False)
+                pass
+            else:
+
+                Tienda = Obtener_Tienda(Order_id)
+                print(auth_Num + " / " + str(Order_id) + " / " + Tienda)
+                
+                with open(Log, "a") as archivo:
+                    archivo.write(auth_Num + " / " + str(Order_id) + " / " + Tienda + "\n")
+
+                #---Cambiar Tienda---#
+                df_Reporte.at[i,"RESTAURANTE"] = Tienda
+                df_Reporte.at[i,"OBS"] = ""
+
+        i += 1
+    df_Reporte.to_excel(Reporte[:-4]+"Nuevo"+".xlsx",index=False)
+    cursor.close()
+    conecction.close()
+    input("Presiona Enter para salir...")
